fc55a8fec500ff1b7e7fc933a7c38d1c7a4ee1ec
Salvo Russo <s.russo@prismaprogetti.it>
2020-12-15 23:00:29 +0100

    Spring Boot App Setup

---
+++ orders/src/main/java/com/unict/dsbd/orders/OrdersApplication.java
@@ -0,0 +1,24 @@
+package com.unict.dsbd.orders;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersApplication {
+	
+	public static String version;
+	public static final Logger log = LoggerFactory.getLogger(OrdersApplication.class);
+	
+	public OrdersApplication(@Value("${appVersion}") String v) {
+		version=v;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersApplication.class, args);
+		log.info("OrdersApplication up and running, version: "+ version);
+	}
+
+}
