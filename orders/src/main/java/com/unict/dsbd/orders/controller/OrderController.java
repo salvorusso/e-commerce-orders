c24cc033ab4d1590f28e6ffdbbb2bb7f413cee67
Salvo Russo <s.russo@prismaprogetti.it>
2020-12-18 19:26:34 +0100

    Changed annotation to RestController
    Annotando con RestController tutti i metodi hanno implicitamente ResponseBody

--- orders/src/main/java/com/unict/dsbd/orders/controller/OrderController.java
+++ orders/src/main/java/com/unict/dsbd/orders/controller/OrderController.java
@@ -5,25 +5,21 @@ import com.unict.dsbd.orders.order.Order;
 import com.unict.dsbd.orders.order.OrderRepository;
 import com.unict.dsbd.orders.product.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.*;
-import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
 
-@org.springframework.stereotype.Controller
+@RestController
 public class OrderController {
 
     @Autowired
     OrderRepository repo;
 
     @GetMapping(path = "/orders/{id}")
-    public @ResponseBody ResponseEntity getOrderById(@PathVariable int id){
+    public ResponseEntity getOrderById(@PathVariable int id){
         Order o1 = repo.findById(id);
         if(o1 != null)
             return ResponseEntity.ok(o1);
@@ -32,7 +28,7 @@ public class OrderController {
     }
 
     @GetMapping(path = "/orders")
-    public @ResponseBody ResponseEntity getOrders(){
+    public ResponseEntity getOrders(){
         /*TO DO::   - implementare passaggio userID al metodo findAllById().
         *           - Implementare meccanismo di impaginazione per_Page
         * */
@@ -46,7 +42,7 @@ public class OrderController {
 
 
     @RequestMapping(path="/insert")
-    public @ResponseBody String testInsert(){
+    public String testInsert(){
         Product p1 = new Product(2,2);
         List<Product> l1= new ArrayList<Product>();
         l1.add(p1);
