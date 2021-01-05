3c88e00c0326934fcf4172217734fc26051985c3
Salvo Russo <s.russo@prismaprogetti.it>
2021-01-05 18:43:15 +0100

    Aggiunto Container per ambiente python di test e debug

--- scripts/build_and_deploy.sh
+++ scripts/build_and_deploy.sh
@@ -3,6 +3,10 @@ echo "Building ordermanager v1.0.0..."
 cd ../orders
 docker build --no-cache -t unict/ordermanager:1.0.0 -f Dockerfile .
 
+echo "Building fake_producer v0.0.1..."
+cd ../fake_producer
+docker build --no-cache -t unict/fake_producer:0.0.1 -f Dockerfile .
+
 echo "Deploying with compose..."
 cd ../scripts
 docker-compose -p order-service -f docker-compose.yaml --env-file .env up -d
