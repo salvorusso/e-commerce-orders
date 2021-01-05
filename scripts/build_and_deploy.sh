102b5352a5c3d0697d2c5d9c7df00233da3eeffd
Salvo Russo <s.russo@prismaprogetti.it>
2021-01-05 19:17:51 +0100

    rimossa cartella inutile, spostato Dockerfile di test debug kafka

--- scripts/build_and_deploy.sh
+++ scripts/build_and_deploy.sh
@@ -4,9 +4,9 @@ cd ../orders
 docker build --no-cache -t unict/ordermanager:1.0.0 -f Dockerfile .
 
 echo "Building fake_producer v0.0.1..."
-cd ../fake_producer
+cd ../scripts
 docker build --no-cache -t unict/fake_producer:0.0.1 -f Dockerfile .
 
 echo "Deploying with compose..."
-cd ../scripts
+#cd ../scripts
 docker-compose -p order-service -f docker-compose.yaml --env-file .env up -d
